b49b6ad0a8c93d057dd8e5c71e38fd1d9cbe6b8c
TBaoBrian09 <baont0209@gmail.com>
2022-04-13 16:50:09 +0700

    add

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes } from "react-router-dom";
-import {} from '/'
+import { AuthProvider } from "./contexts/auth-context";
 
 function App() {
   return (
--- src/contexts/auth-context.js
+++ src/contexts/auth-context.js
@@ -1,16 +1,14 @@
-const { createContext, useContext, useState } = require("react");
+import { createContext, useContext } from "react";
 
 const AuthContext = createContext();
-
 function AuthProvider(props) {
-  const [userInfo, setUserInfo] = useState({});
-  const value = { userInfo, setUserInfo };
-  return <AuthContext.Provider value={value} {...props}></AuthContext.Provider>;
+  return <AuthContext.Provider {...props}></AuthContext.Provider>;
 }
+
 function useAuth() {
   const context = useContext(AuthContext);
   if (typeof context === "undefined")
-    throw new Error("useAuth must be used within AuthProvider");
-  return context;
+    throw new Error("useAth must be used within AuthProvider");
 }
+
 export { AuthProvider, useAuth };
