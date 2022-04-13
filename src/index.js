345bfdfe1c8a33b3f287b0ebc3bb4e0972390cd6
TBaoBrian09 <baont0209@gmail.com>
2022-04-13 16:31:20 +0700

    add

--- src/index.js
+++ src/index.js
@@ -4,17 +4,22 @@ import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-import {} from "./styles/G";
 
 import { theme } from "./utils/cons";
+import { GlobalStyles } from "./styles/GlobalStyles";
+import { ThemeProvider } from "styled-components";
+
 const container = document.getElementById("app");
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles></GlobalStyles>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 );
 
